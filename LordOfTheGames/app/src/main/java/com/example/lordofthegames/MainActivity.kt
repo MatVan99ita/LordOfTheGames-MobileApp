bbf3fbaf1dee571119dd26b0672869e3962f6b15
MatVan99ita <matteo.vanni2@studio.unibo.it>
2022-10-25 14:54:52 +0200

    Primi passi

--- LordOfTheGames/app/src/main/java/com/example/lordofthegames/MainActivity.kt
+++ LordOfTheGames/app/src/main/java/com/example/lordofthegames/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.lordofthegames
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
     }
 }
