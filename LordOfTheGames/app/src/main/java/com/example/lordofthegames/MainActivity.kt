8b32406758cf7c3a1bfe99373aa070d060b896ef
MatVan99ita <matteo.vanni2@studio.unibo.it>
2022-10-25 15:39:27 +0200

    Finita la topbar

--- LordOfTheGames/app/src/main/java/com/example/lordofthegames/MainActivity.kt
+++ LordOfTheGames/app/src/main/java/com/example/lordofthegames/MainActivity.kt
@@ -7,6 +7,6 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home);
     }
 }
