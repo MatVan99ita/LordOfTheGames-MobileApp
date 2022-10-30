15670ce29675938a5c73c0e946798616a44efe52
MatVan99ita <56399084+MatVan99ita@users.noreply.github.com>
2022-10-30 09:39:13 +0100

    si scosa

--- LordOfTheGames/app/src/main/java/com/example/lordofthegames/MainActivity.kt
+++ LordOfTheGames/app/src/main/java/com/example/lordofthegames/MainActivity.kt
@@ -1,29 +1,47 @@
 package com.example.lordofthegames
 
-import android.app.Activity
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Button
-import androidx.recyclerview.widget.RecyclerView
-import com.example.lordofthegames.RecyclerView.CardAdapter
-import com.example.lordofthegames.RecyclerView.CardItem
+
 
 class MainActivity : AppCompatActivity() {
 
+    var simpleGrid: GridView? = null
+    var logos = intArrayOf(
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground
+    )
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home);
+        setContentView(R.layout.grid_layout);
 
-        setRecyclerView(this)
+        //setRecyclerView(this)
     }
 
-    lateinit var adapter: CardAdapter
+    /*lateinit var adapter: CardAdapter
     lateinit var recyclerView: RecyclerView
     private fun setRecyclerView(mainActivity: Activity) {
-        recyclerView = mainActivity.findViewById(R.id.recycler_view)
+        recyclerView = mainActivity.findViewById(R.id.home)
         recyclerView.setHasFixedSize(true)
         val list: List<CardItem> = listOf(CardItem("ic_baseline_android_24", "GIUOCO"))
         adapter = CardAdapter(list, mainActivity)
         recyclerView.adapter = adapter
-    }
+    }*/
 }
