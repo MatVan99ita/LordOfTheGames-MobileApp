9664bdbdc95be6a3e02681368297ddf8650f4607
MatVan99ita <56399084+MatVan99ita@users.noreply.github.com>
2022-11-22 17:56:33 +0100

    piccolo todo

--- LordOfTheGames/app/src/main/java/com/example/lordofthegames/CONVERSIONE_J_K.kt
+++ LordOfTheGames/app/src/main/java/com/example/lordofthegames/CONVERSIONE_J_K.kt
@@ -8,6 +8,12 @@ import com.example.lordofthegames.recyclerView.CardItem
 
 class CONVERSIONE_J_K {
 
+    /**
+     * PRATICAMENTE DEVO USARE LA RECYCLER PERO' COME LAYOUT DEVO METTERGLI LA GRID PER COME VUOLE LUI IN MODO DA POTERLA USARE FREEBOOTANDO IL CODICE DEL LAB
+     * IN PIU' DEVO SISTEMARE I FRAGMENT IN MODO CHE PRATICAMENTE PARTANO ALTRIMENTI RIMANE TUTTO VUOTO
+     */
+
+
     /*fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val activity: Activity = getActivity()
