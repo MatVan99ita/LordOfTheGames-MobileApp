6c196f476fd8d8def45a5b9c5c34b044793129fc
MatVan99ita <56399084+MatVan99ita@users.noreply.github.com>
2022-11-22 17:57:56 +0100

    eliminazione codice convertito

--- LordOfTheGames/app/src/main/java/com/example/lordofthegames/CONVERSIONE_J_K.kt
+++ LordOfTheGames/app/src/main/java/com/example/lordofthegames/CONVERSIONE_J_K.kt
@@ -14,55 +14,5 @@ class CONVERSIONE_J_K {
      */
 
 
-    /*fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val activity: Activity = getActivity()
-        if (activity != null) {
-            Utilities.setUpToolBar(activity as AppCompatActivity, getString(R.string.settings))
-            val textInputLayout: TextInputLayout = view.findViewById(R.id.username_textinput)
-            val editText = textInputLayout.editText
-            val textView: TextView = view.findViewById(R.id.username_textview)
-            val sharedPreferences = activity.getPreferences(Context.MODE_PRIVATE)
-            textView.text = sharedPreferences.getString("Settings", "username")
-            editText!!.addTextChangedListener(object : TextWatcher {
-                override fun beforeTextChanged(
-                    charSequence: CharSequence,
-                    i: Int,
-                    i1: Int,
-                    i2: Int
-                ) {
-                }
-
-                override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
-                override fun afterTextChanged(editable: Editable) {
-                    textView.text = editable
-                    val editor = sharedPreferences.edit()
-                    editor.putString("Settings", editable.toString())
-                    editor.apply()
-                }
-            })
-        }
-    }*
-
-    protected fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) Utilities.insertFragment(
-            this, HomeFragment(),
-            HomeFragment::class.java.simpleName
-        )
-        addViewModel = ViewModelProvider(this).get(AddViewModel::class.java)
-    }// *
-
-}
-
-class ListViewModel(application: Application) : AndroidViewModel(application) {
-    val cardItems: LiveData<List<CardItem>>
-
-    init {
-        val repository = CardItemRepository(application)
-        cardItems = repository.getCardItemList()
-    }
-} // */
 }
 
