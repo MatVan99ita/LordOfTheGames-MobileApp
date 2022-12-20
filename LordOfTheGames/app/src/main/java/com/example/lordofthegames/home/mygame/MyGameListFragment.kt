fec136a2e8a56d2bd71d1b8fb7d15261ef86a23f
MatVan99ita <matteo.vanni2@studio.unibo.it>
2022-12-20 11:44:50 +0100

    altri test iniziali per il tabbed

--- LordOfTheGames/app/src/main/java/com/example/lordofthegames/home/mygame/MyGameListFragment.kt
+++ LordOfTheGames/app/src/main/java/com/example/lordofthegames/home/mygame/MyGameListFragment.kt
@@ -1,10 +1,45 @@
 package com.example.lordofthegames.home.mygame
 
+
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import com.example.lordofthegames.R
+import com.google.android.material.tabs.TabLayout
+
 
 class MyGameListFragment: Fragment() {
 
+    private var viewPager: ViewPager? = null
+    private var sectionsPagerAdapter: SectionsPagerAdapter? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sectionsPagerAdapter = context?.let { cntx -> activity?.supportFragmentManager?.let { actFragMng ->
+                SectionsPagerAdapter(cntx, actFragMng)
+            }
+        }
+        //val viewPager: ViewPager = view?.findViewById(R.id.view_pager) as ViewPager
+
+        context?.let { TabLayout(it) }?.setupWithViewPager(viewPager)
+        val tabs: TabLayout? = context?.let { TabLayout(it) }
+        tabs?.setupWithViewPager(viewPager)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view: View = inflater.inflate(R.layout.fragment_mygame, container, false)
+        viewPager = view.findViewById<View>(R.id.view_pager) as ViewPager
+        viewPager?.adapter = sectionsPagerAdapter
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
     companion object {
         /**
