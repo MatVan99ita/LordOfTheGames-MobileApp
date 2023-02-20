b9f5d4db3dbc8a25162dbff51acd9fafbc553034
MatVan99ita <matteo.vanni2@studio.unibo.it>
2023-02-20 15:15:52 +0100

    creata la note funzionante

--- LordOfTheGames/app/src/main/java/com/example/lordofthegames/GameDetails/NotesFragment.kt
+++ LordOfTheGames/app/src/main/java/com/example/lordofthegames/GameDetails/NotesFragment.kt
@@ -1,27 +1,47 @@
 package com.example.lordofthegames.GameDetails
 
+import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.*
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.lordofthegames.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class NotesFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        //return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_game_notes, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val activity: Activity? = activity
+
+        if(activity != null){
+            val insert: TextInputEditText =  view.findViewById(R.id.noteInsert)
+            insert.hint = "Insert note here"
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
 
