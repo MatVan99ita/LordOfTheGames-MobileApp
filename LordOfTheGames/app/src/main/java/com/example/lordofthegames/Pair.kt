7adf68bedf7733f06c892475e86214cfcabbfc24
MatVan99ita <56399084+MatVan99ita@users.noreply.github.com>
2023-09-22 16:38:44 +0200

    Aggiunto Pair

---
+++ LordOfTheGames/app/src/main/java/com/example/lordofthegames/Pair.kt
@@ -0,0 +1,37 @@
+package com.example.lordofthegames
+
+
+class Pair<X, Y>(x: X, y: Y) {
+    val x: X?
+    val y: Y?
+
+    init {
+        this.x = x
+        this.y = y
+    }
+
+    override fun hashCode(): Int {
+        val prime = 31
+        var result = 1
+        result = prime * result + (x?.hashCode() ?: 0)
+        result = prime * result + (y?.hashCode() ?: 0)
+        return result
+    }
+
+    override fun equals(obj: Any?): Boolean {
+        if (this === obj) return true
+        if (obj == null) return false
+        if (javaClass != obj.javaClass) return false
+        val other = obj as Pair<*, *>
+        if (x == null) {
+            if (other.x != null) return false
+        } else if (x != other.x) return false
+        return if (y == null) {
+            other.y == null
+        } else y == other.y
+    }
+
+    override fun toString(): String {
+        return "Pair [x=$x, y=$y]"
+    }
+}
