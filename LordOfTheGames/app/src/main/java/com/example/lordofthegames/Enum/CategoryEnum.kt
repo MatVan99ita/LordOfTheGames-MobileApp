f5d45f992425a18a71be5ce21448831459c8ecfa
MatVan99ita <56399084+MatVan99ita@users.noreply.github.com>
2023-09-22 16:22:26 +0200

    creato enum category

--- LordOfTheGames/app/src/main/java/com/example/lordofthegames/Enum/CategoryEnum.kt
+++ LordOfTheGames/app/src/main/java/com/example/lordofthegames/Enum/CategoryEnum.kt
@@ -1,2 +1,24 @@
 package com.example.lordofthegames.Enum
 
+import com.example.lordofthegames.db_entities.Categories
+
+enum class CategoryEnum(category: String){
+     SD("Story-driven"),
+     SL("Souls-like"),
+     STR("Strategico"),
+     GDR("GDR"),
+     ACT("Azione"),
+     AVV("Avventura"),
+     OW("Open world"),
+     SIM("Simulativo"),
+     HNS("Hack&Slash"),
+     FNT("Fantasy"),
+     TP("Third person"),
+     TPS("TPS"),
+     FPS("FPS"),
+     DF("Dark fantasy"),
+     HRR("Horror");
+
+}
+
+
