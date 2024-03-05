eeca3cca349d941f461fd93a82c2ca7be27da7ac
MatVan99ita <matteo.vanni2@studio.unibo.it>
2024-03-05 17:33:15 +0100

    Update PlatformCardItem.kt

--- LordOfTheGames/app/src/main/java/com/example/lordofthegames/RecyclerView/PlatformCardItem.kt
+++ LordOfTheGames/app/src/main/java/com/example/lordofthegames/RecyclerView/PlatformCardItem.kt
@@ -1,4 +1,9 @@
 package com.example.lordofthegames.recyclerView
 
+import okhttp3.internal.toHexString
+
 class PlatformCardItem(var platFormName: String, var color: Int) {
+    override fun toString(): String {
+        return "$platFormName - $color - ${color.toHexString()}"
+    }
 }
