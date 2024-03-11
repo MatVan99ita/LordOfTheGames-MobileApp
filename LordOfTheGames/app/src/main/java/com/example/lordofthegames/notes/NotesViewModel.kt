8d83092b369f61fac43ff7fc34fbf56d72ec448a
MatVan99ita <matteo.vanni2@studio.unibo.it>
2024-03-11 15:37:51 +0100

    Eliminazione package note inutile

--- LordOfTheGames/app/src/main/java/com/example/lordofthegames/notes/NotesViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lordofthegames.notes
-
-import android.app.Application
-import com.example.lordofthegames.Database.AbstractViewModel
-
-class NotesViewModel(application: Application): AbstractViewModel(application) {
-}
