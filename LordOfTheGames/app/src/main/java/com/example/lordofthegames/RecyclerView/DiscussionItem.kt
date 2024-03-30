5279c6c403df9e3b5c18e4a21a68e0be246bcbfd
MatVan99ita <56399084+MatVan99ita@users.noreply.github.com>
2024-03-30 12:04:38 +0100

    roba delle discussioni

--- LordOfTheGames/app/src/main/java/com/example/lordofthegames/RecyclerView/DiscussionItem.kt
+++ LordOfTheGames/app/src/main/java/com/example/lordofthegames/RecyclerView/DiscussionItem.kt
@@ -1,9 +1,7 @@
 package com.example.lordofthegames.recyclerView
 
-class DiscussionItem(
-    val discussion_id: Int,
-    val title: String,
-    val content: String,
-    val TotalLike: Int,
-    val NumeroCommenti: Int){
-}
+import com.example.lordofthegames.db_entities.Discussion
+
+
+class DiscussionItem(val discussion: Discussion, val totaleLike: Int, val totComment: Int)
+
---
+++ LordOfTheGames/app/src/main/java/com/example/lordofthegames/RecyclerView/DiscussionSpecificAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.lordofthegames.recyclerView
+
+import android.app.Activity
+import android.app.Application
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import com.example.lordofthegames.R
+
+class DiscussionSpecificAdapter(var activity: Activity, var listener: OnItemListener, var list: List<DiscussionItem>):
+    RecyclerView.Adapter<DiscussionSpecificAdapter.DiscussionSpecificHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DiscussionSpecificHolder {
+        return DiscussionSpecificHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.discussion_item,
+                parent,
+                false
+            ), listener
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: DiscussionSpecificHolder, position: Int) {
+        val item: DiscussionItem = list[position]
+    }
+
+
+
+    inner class DiscussionSpecificHolder(itemView: View, lister: OnItemListener): RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        override fun onClick(p0: View?) {
+            TODO("Not yet implemented")
+        }
+    }
+}
