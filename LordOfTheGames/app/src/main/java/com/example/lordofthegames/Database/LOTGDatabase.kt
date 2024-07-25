4d59881db13706d624a0b21867c1b9a15aa542c5
MatVan99ita <matteo.vanni2@studio.unibo.it>
2024-07-25 10:59:01 +0200

    Update LOTGDatabase.kt

--- LordOfTheGames/app/src/main/java/com/example/lordofthegames/Database/LOTGDatabase.kt
+++ LordOfTheGames/app/src/main/java/com/example/lordofthegames/Database/LOTGDatabase.kt
@@ -7,7 +7,6 @@ import com.example.lordofthegames.db_entities.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-
 @Database(entities = [
             Achievement::class,
             Categories::class,
@@ -26,73 +25,21 @@ import java.util.concurrent.Executors
 abstract class LOTGDatabase: RoomDatabase() {
 
     abstract fun lotgdao(): LotgDao
-
-    //abstract fun userDao(): UserDAO
-
-
     companion object {
-
-
         @Volatile
         private var INSTANCE: LOTGDatabase? = null
-
         val executor: ExecutorService = Executors.newFixedThreadPool(4)
-
         fun getDatabase(context: Context): LOTGDatabase {
-
             return synchronized(this){
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     LOTGDatabase::class.java,
-                    "lotgdb"
-                    )
+                    "lotgdb")
                     .createFromAsset("lotgdb.db")
                     .allowMainThreadQueries()
                     .build()
-
                 instance
             }
         }
     }
 }
-
-
-
-    /**
-     *  -- Inserimento del nuovo videogioco nella tabella "Videogioco"
-     *  INSERT INTO Videogioco (titolo)
-     *  VALUES ('Super Mario Bros.');
-     *
-     *  -- Recupero l'ID del nuovo videogioco
-     *  SET @id_videogioco = LAST_INSERT_ID();
-     *
-     *  -- Inserimento delle associazioni tra il nuovo videogioco e le categorie nella tabella "VideogiocoCategoria"
-     *  INSERT INTO VideogiocoCategoria (id_videogioco, id_categoria)
-     *  VALUES (@id_videogioco, 1),
-     *  (@id_videogioco, 2);
-     */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
